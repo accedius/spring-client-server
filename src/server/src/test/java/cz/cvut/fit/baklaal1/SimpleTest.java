c459052eac69404fc6344a24bbd39e229df3f514
Aleksej Baklanov <baklaal1@cvut.cz>
2020-11-05 00:53:48 +0100

    Added basic test

---
+++ src/server/src/test/java/cz/cvut/fit/baklaal1/SimpleTest.java
@@ -0,0 +1,21 @@
+package cz.cvut.fit.baklaal1;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SimpleTest {
+    @Test
+    public void passingTest() {
+        assertEquals(123,123);
+    }
+
+    @Test
+    @AfterAll
+    static void consoleInfo() {
+        System.out.println("SimpleTest Passed!");
+    }
+}
