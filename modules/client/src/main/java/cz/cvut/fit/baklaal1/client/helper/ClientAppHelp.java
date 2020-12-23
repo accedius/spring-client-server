6e291ff92341b8bf11c7e3cba82468be4e17aef2
Aleksei Baklanov <baklaal1@fit.cvut.cz>
2020-12-23 23:54:42 +0100

    Add Placeholder for Manual/Help providing module

---
+++ modules/client/src/main/java/cz/cvut/fit/baklaal1/client/helper/ClientAppHelp.java
@@ -0,0 +1,10 @@
+package cz.cvut.fit.baklaal1.client.helper;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ClientAppHelp {
+    public void printHelp() {
+        ;
+    }
+}
