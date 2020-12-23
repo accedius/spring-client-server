77058f65fcc7405d6dbf21a2ed145b286f2204d6
Aleksei Baklanov <baklaal1@fit.cvut.cz>
2020-12-23 23:53:59 +0100

    Edit/Update ClientApp

--- modules/client/src/main/java/cz/cvut/fit/baklaal1/client/ClientApp.java
+++ modules/client/src/main/java/cz/cvut/fit/baklaal1/client/ClientApp.java
@@ -1,17 +1,79 @@
 package cz.cvut.fit.baklaal1.client;
 
+import cz.cvut.fit.baklaal1.client.handler.AssessmentHandler;
+import cz.cvut.fit.baklaal1.client.handler.StudentHandler;
+import cz.cvut.fit.baklaal1.client.handler.TeacherHandler;
+import cz.cvut.fit.baklaal1.client.handler.WorkHandler;
+import cz.cvut.fit.baklaal1.client.helper.ClientAppHelp;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.client.RestTemplateCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.hateoas.config.EnableHypermediaSupport;
+import org.springframework.hateoas.config.HypermediaRestTemplateConfigurer;
 
 @SpringBootApplication
 @EntityScan("cz.cvut.fit.baklaal1.model.data.entity")
 @EnableHypermediaSupport(type = EnableHypermediaSupport.HypermediaType.HAL)
-public class ClientApp {
+public class ClientApp implements ApplicationRunner {
+	@Autowired
+	private AssessmentHandler assessmentHandler;
+
+	@Autowired
+	private StudentHandler studentHandler;
+
+	@Autowired
+	private TeacherHandler teacherHandler;
+
+	@Autowired
+	private WorkHandler workHandler;
+
+	@Autowired
+	private ClientAppHelp helpModule;
 
     public static void main(String[] args) {
         SpringApplication.run(ClientApp.class, args);
     }
 
+    @Bean
+    RestTemplateCustomizer hypermediaRestTemplateCustomizer(HypermediaRestTemplateConfigurer configurer) {
+		return restTemplate -> {
+			configurer.registerHypermediaTypes(restTemplate);
+		};
+	}
+
+    @Override
+    public void run(ApplicationArguments args) {
+        if (args.containsOption("action") && args.containsOption("entity")) {
+        	String entity = args.getOptionValues("entity").get(0);
+			switch (entity) {
+				case "Student": {
+					studentHandler.handle(args);
+					break;
+				}
+				case "Teacher": {
+					teacherHandler.handle(args);
+					break;
+				}
+				case "Work": {
+					workHandler.handle(args);
+					break;
+				}
+				case "Assessment": {
+					assessmentHandler.handle(args);
+					break;
+				}
+				default: {
+					System.err.println("Invalid entity name: \"" + entity + "\"!");
+					helpModule.printHelp();
+				}
+			}
+		} else {
+        	helpModule.printHelp();
+		}
+    }
 }
