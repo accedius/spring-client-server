192c400cfc099cc3fb892d1f541fd0c33ccdd8f5
Aleksei Baklanov <baklaal1@fit.cvut.cz>
2020-12-24 12:43:59 +0100

    Add Client's Help manual class placeholder

--- modules/client/src/main/java/cz/cvut/fit/baklaal1/client/helper/ClientAppHelp.java
+++ modules/client/src/main/java/cz/cvut/fit/baklaal1/client/helper/ClientAppHelp.java
@@ -4,7 +4,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ClientAppHelp {
-    public void printHelp() {
-        ;
+    public static void printHelp() {
+        //TODO
+        System.out.println("Printing Help...");
     }
 }
